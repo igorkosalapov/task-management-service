5bfe33b8418471904a363515f4fc5fd111a01894
Igor Kosalapov <ikosalapov@yandex.ru>
2026-06-20 20:48:47 +0500

    test: настроено управление жизненным циклом Testcontainers

--- src/test/java/org/example/integration/TaskFlowIntegrationTest.java
+++ src/test/java/org/example/integration/TaskFlowIntegrationTest.java
@@ -20,19 +20,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 import java.time.Duration;
@@ -51,38 +48,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Testcontainers
 @ActiveProfiles("test")
+@Import(TestcontainersConfiguration.class)
 class TaskFlowIntegrationTest {
-
-    @Container
-    static final PostgreSQLContainer<?> POSTGRESQL =
-            new PostgreSQLContainer<>("postgres:16-alpine");
-
-    @Container
-    static final KafkaContainer KAFKA = new KafkaContainer(
-            DockerImageName.parse("confluentinc/cp-kafka:7.6.1")
-    );
-
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add(
-                "spring.datasource.url",
-                POSTGRESQL::getJdbcUrl
-        );
-        registry.add(
-                "spring.datasource.username",
-                POSTGRESQL::getUsername
-        );
-        registry.add(
-                "spring.datasource.password",
-                POSTGRESQL::getPassword
-        );
-        registry.add(
-                "spring.kafka.bootstrap-servers",
-                KAFKA::getBootstrapServers
-        );
-    }
 
     @Autowired
     private MockMvc mockMvc;
@@ -98,6 +66,9 @@ class TaskFlowIntegrationTest {
 
     @Autowired
     private KafkaTemplate<String, TaskEvent> kafkaTemplate;
+
+    @Autowired
+    private KafkaContainer kafkaContainer;
 
     private Consumer<String, TaskEvent> consumer;
 
@@ -299,7 +270,7 @@ class TaskFlowIntegrationTest {
 
         properties.put(
                 ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                KAFKA.getBootstrapServers()
+                kafkaContainer.getBootstrapServers()
         );
 
         properties.put(
